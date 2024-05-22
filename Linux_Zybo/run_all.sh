e86ca7b4f97efa3b22b3455691c1f1f0eff7192c
Vincenzo Maisto <vincenzo.maisto2@unina.it>
2024-05-22 13:02:08 +0200

    Re-order and rename build scripts

--- Linux_Zybo/run_all.sh
+++ Linux_Zybo/run_all.sh
@@ -1,7 +1,7 @@
 source 0_install_prerequisites.sh
-source 1_cross_compile_kernel.sh
-source 2_get_fsbl.sh
-source 3_cross_compile_u-boot.sh
+source 1_get_fsbl.sh
+source 2_cross_compile_u-boot.sh
+source 3_cross_compile_kernel.sh
 source 4_generate_bootimage.sh
 source 5_device_tree.sh
 source 6_cross_compile_rootfs.sh
